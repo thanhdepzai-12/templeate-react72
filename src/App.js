3de5103045de9f303236055e1c577c1b8d5c4a81
Thanh Bieeng <mrthanh20069@gmail.com>
2024-06-16 23:28:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+const App = () => {
+  return (
+    <div className="App-container">
+<Header />
+    </div>
+  );
+}
+
+export default App;
